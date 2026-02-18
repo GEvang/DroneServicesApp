5589bf217c76e070c53cc44078c6e75fb037492e
GEvang <GEvang97@outlook.com>
2026-02-18 16:24:24 +0200

    Remove test navigation; add Greek telemetry string
    Removed the testing navigation destination from the nav graph and AppBarConfiguration so it's no longer treated as a top-level destination. Added a Greek localized string for a telemetry-forwarding warning.
    
    Modified files:
    - MainActivity.kt: removed R.id.nav_test from AppBarConfiguration
    - mobile_navigation.xml: removed nav_test fragment
    - values-el/strings.xml: added no_telemetry_forwarding_msg

--- app/src/main/java/com/example/droneservicesapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/droneservicesapp/activities/MainActivity.kt
@@ -113,7 +113,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_maps_home, R.id.nav_settings, R.id.nav_test
+                R.id.nav_maps_home, R.id.nav_settings
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
