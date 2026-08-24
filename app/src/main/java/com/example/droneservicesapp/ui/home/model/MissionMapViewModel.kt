0e959dce4e5b9f5f23ebc994a4d2cc32800dec60
GEvang <GEvang97@outlook.com>
2026-08-24 13:40:22 +0300

    Revert "Keep workflow panel available in planning"
    This reverts commit fc53d7542ed0e95a2c9563785125cd44576fe299.

--- app/src/main/java/com/example/droneservicesapp/ui/home/model/MissionMapViewModel.kt
+++ app/src/main/java/com/example/droneservicesapp/ui/home/model/MissionMapViewModel.kt
@@ -6,7 +6,7 @@ import com.example.droneservicesapp.ui.shell.model.MainActivityViewModel
 
 class MissionMapViewModel : ViewModel() {
     private var currentScreenMode: HomeMapScreenMode = HomeMapScreenMode.Idle
-    private var isPlanningPanelVisible: Boolean = true
+    private var isPlanningPanelVisible: Boolean = false
     private var hasMissionArea: Boolean = false
     private var arePanelsDismissed: Boolean = false
 
@@ -44,6 +44,11 @@ class MissionMapViewModel : ViewModel() {
     }
 
     fun togglePlanningPanelVisible() {
+        val hasVisibleSidePanel = isPlanningPanelVisible || (hasMissionArea && !arePanelsDismissed)
+        if (hasVisibleSidePanel) {
+            dismissSidePanels()
+            return
+        }
         isPlanningPanelVisible = true
         arePanelsDismissed = false
         publishState()
